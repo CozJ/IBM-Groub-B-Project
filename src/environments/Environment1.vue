9a7aca975d4ff53327500ea3cc2ffb0d22097ddf
b9038830 <b9038830@my.shu.ac.uk>
2021-01-12 01:31:43 +0900

    Set environment 1 to have the generated environment

--- src/environments/Environment1.vue
+++ src/environments/Environment1.vue
@@ -1,16 +1,13 @@
 <template>
   <a-box id="test-target-one" body="shape: box" hoverable grabbable draggable droppable color="red" position="0 5 -3" rotation="0 45 45" scale="1 1 1"></a-box>
   <a-entity body="type: static; shape: box" obj-model="obj: #obj; mtl: #mtl;" position="-3 2 0" scale="5 5 5"></a-entity>
-  <a-entity cubemap="folder: /SkyBoxes/"></a-entity>
+  <a-entity environment="preset: starry"></a-entity>
 
   <!-- Ground collider -->
   <a-box body="type: static; shape: box" width=100 height=1 depth=100 position="0 -0.8 0" visible="false"></a-box>
 
-  <a-light color="#aa8" position="10 10 0"></a-light>
-  <a-light color="#aa8" position="0 10 10"></a-light>
-  <a-light color="#aa8" position="0 10 10"></a-light>
-  <a-light color="#aa8" position="10 0 10"></a-light>
-  <a-light color="#aa8" position="10 10 0"></a-light>
+  <a-light color="#ffc" position="20 20 0"></a-light>
+  <a-light color="#aa8" position="-20 10 -10"></a-light>
 
   <a-assets>
     <a-assets-item id="mtl" :src="require('../assets/Room/materials.mtl')"></a-assets-item>
