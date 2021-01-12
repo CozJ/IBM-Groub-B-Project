9d4b22a98e1999227c87b27156139fb9531ec2f8
b9038830 <b9038830@my.shu.ac.uk>
2021-01-12 20:44:26 +0900

    Test new generated navmesh workflow

--- src/environments/Environment1.vue
+++ src/environments/Environment1.vue
@@ -1,7 +1,7 @@
 <template>
   <!--Assets-->
   <a-assets>
-    <a-assets-item id="asset-mtl" :src="require('../assets/Room/materials.mtl')"></a-assets-item>
+    <a-assets-item id="asset-mtl" :src="require('../assets/Room/model.mtl')"></a-assets-item>
     <a-assets-item id="asset-obj" :src="require('../assets/Room/model.obj')"></a-assets-item>
     <a-assets-item id="asset-navmesh" :src="require('../assets/Room/navmesh.gltf')"></a-assets-item>
   </a-assets>
@@ -11,9 +11,9 @@
 
   <!--Objects-->
   <a-box id="test-target-one" body="shape: box" hoverable grabbable draggable droppable color="red" position="0 5 -3" rotation="0 45 45" scale="1 1 1"></a-box>
-  <a-entity body="type: static; shape: box" obj-model="obj: #asset-obj; mtl: #asset-mtl;" position="-3 2 0" scale="5 5 5"></a-entity>
+  <a-entity body="type: static; shape: box" obj-model="obj: #asset-obj; mtl: #asset-mtl;" position="4 -0.2 0" scale="1 1 1"></a-entity>
 
-  <a-entity gltf-model="#asset-navmesh" nav-mesh visible="false" position="-3 2 0" scale="5 5 5"></a-entity>
+  <a-entity gltf-model="#asset-navmesh" nav-mesh visible="true" position="4 -0.2 0" scale="1 1 1"></a-entity>
 
   <!-- Ground collider -->
   <a-box body="type: static; shape: box" width=100 height=1 depth=100 position="0 -0.8 0" visible="false"></a-box>
