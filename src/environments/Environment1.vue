49b61e07bdf45b0dcdccd87c0f7a28b786f85228
Corey <corey.j.sutcliffe@gmail.com>
2021-01-12 09:32:48 +0000

    refactored SkyBox Folder, added comments to env1/2

--- src/environments/Environment1.vue
+++ src/environments/Environment1.vue
@@ -1,13 +1,19 @@
 <template>
+
+  <!--SkyBox-->
+  <a-entity environment="preset: starry"></a-entity>
+
+  <!--Objects-->
   <a-box id="test-target-one" body="shape: box" hoverable grabbable draggable droppable color="red" position="0 5 -3" rotation="0 45 45" scale="1 1 1"></a-box>
   <a-entity body="type: static; shape: box" obj-model="obj: #obj; mtl: #mtl;" position="-3 2 0" scale="5 5 5"></a-entity>
-  <a-entity environment="preset: starry"></a-entity>
 
   <!-- Ground collider -->
   <a-box body="type: static; shape: box" width=100 height=1 depth=100 position="0 -0.8 0" visible="false"></a-box>
-
+  
+  <!--lights-->
   <a-light type="ambient" intensity="0.2" color="#fff" position="20 20 0"></a-light>
 
+  <!--Assets-->
   <a-assets>
     <a-assets-item id="mtl" :src="require('../assets/Room/materials.mtl')"></a-assets-item>
     <a-assets-item id="obj" :src="require('../assets/Room/model.obj')"></a-assets-item>
--- src/environments/Environment2.vue
+++ src/environments/Environment2.vue
@@ -1,9 +1,18 @@
 <template>
+  <!--SkyBox-->
+  <a-entity cubemap="folder: /SpaceSkyBox/"></a-entity>
+  
+  <!--Objects-->
   <a-box color="green" position="0 2 -5" rotation="0 45 45" scale="2 2 2"></a-box>
-  <a-entity cubemap="folder: /SkyBoxes/"></a-entity>
 
+  <!-- Ground collider -->
+  <a-box body="type: static; shape: box" width=100 height=1 depth=100 position="0 -0.8 0" visible="false"></a-box>
+
+  <!--Lights-->
   <a-light type="point" intensity="1.5" color="#fff" position="-200 -140 -70"></a-light>
   <a-light type="ambient" intensity="0.3" color="#fff" position="20 20 0"></a-light>
+
+  <!--Assets-->
 
 </template>
 
